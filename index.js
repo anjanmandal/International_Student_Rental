c0bdc99ec7adf8f5233929c6a8d6e92e73b17751
Anjan Mandal <anjanmandal2076@gmail.com>
2024-05-15 02:32:46 -0500

    used new features like ejs-mate

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 let express= require("express");
 const app= express();
+const path=require("path");
 const listing = require("./models/listing.js")
 const mongoose=require("mongoose");
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+
+app.use(express.urlencoded({extended:true}));
+
+//---------for delete request method_override
+const methodOverride=require('method-override');
+app.use(methodOverride('_method'));
+
+//------------using ejs mate to create template for common in all pages like nav-bar and footer etc.....
+const ejsMate=require("ejs-mate");
+app.engine("ejs",ejsMate);
+
+//------------for the public folder we are using for the css---------------
+app.use(express.static(path.join(__dirname,"/public")))
+ 
+
 app.listen(8080,()=>{
     console.log(`listening to the port 8080.`)
 })
@@ -11,6 +29,11 @@ main().then(()=>{
 async function main(){
     await mongoose.connect('mongodb://127.0.0.1:27017/Mybnb')
 }
+app.get("/",(req,res)=>{
+    console.log("server is listening....");
+})
+
+//below is the way to add to the mongodb. you cannot use inser here.
 app.get("/testDb",async(req,res)=>{
     let sample= new listing(
         {title: "601 N McGuire Ave",
@@ -23,8 +46,53 @@ app.get("/testDb",async(req,res)=>{
     await sample.save();
     console.log("sample was saved to DB");
     res.send("successfull");
+});
+
+//------------------------will show all the listings--------------------------------
+app.get("/listings",async(req,res)=>{
+    const allListings=await listing.find({});
+    res.render("./listings/index.ejs",{allListings}) 
+});
+
+
+//----------------------details for each listings----------------------------
+app.get("/listings/:id",async(req,res)=>{
+    let {id}=req.params;
+    const item=await listing.find({_id:id}); // or  listing.findById(id)
+    console.log(item);
 })
 
-app.get("/",(req,res)=>{
-    console.log("server is listening....")
+
+//-----------------------getting to add new listings requests------------------------
+app.get("/add-listing",(req,res)=>{
+    res.render("./listings/add.ejs")
+});
+
+//-----------------------adding the new listing------------------
+app.post("/add-listing",async(req,res)=>{
+    let{title,description,imageURL,price,location,country}=req.body;
+    let newItem= new listing(
+        {title:title,
+         description:description,
+         picture:{
+            filename:"",
+            url:imageURL
+         },
+         price:price,
+         location:location,
+         country:country
+        }
+    )
+    await newItem.save();
+    console.log("successfull")
+    res.redirect("/listings");
+});
+
+//------------------------Update listing-------------------------------------
+
+//-------------------------Delete listing------------------------------------
+app.delete("/listings/:id",async(req,res)=>{
+    let {id}=req.params;
+   await listing.deleteOne({_id:id});
+    res.redirect("/listings");
 })
